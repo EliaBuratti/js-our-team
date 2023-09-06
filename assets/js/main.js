12bec15d8c72c396d0f52c038c2f6f81f7b316b5
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-06 14:24:19 +0200

    complete Milestone 0
    create array with information

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,63 @@
-console.log('ciao our team ');
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. 
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+
+BONUS 2:
+Organizzare i singoli membri in card/schede
+
+Consigli del giorno:
+Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico!
+ */
+
+
+
+/* MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite. */
+
+const memberTeam = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',	
+        foto: './assets/img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',	
+        foto: './assets/img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',	
+        foto: './assets/img/walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',	
+        foto: './assets/img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',	
+        foto: './assets/img/scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',	
+        foto: './assets/img/barbara-ramos-graphic-designer.jpg'
+    },
+
+];
+
+console.log(memberTeam);
